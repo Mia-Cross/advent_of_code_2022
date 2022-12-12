ede71225edcced7bce4721abb3df4981bf948064
Leïla MARABESE <lmarabese@scaleway.com>
2022-12-12 05:05:52 +0100

    day 9.1 ok, debugging 9.2

--- day-09_Rope-Bridge/rope_bridge_test.go
+++ day-09_Rope-Bridge/rope_bridge_test.go
@@ -8,11 +8,20 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCountVisitedCells(t *testing.T) {
+func TestCountVisitedCellsWithTwoKnots(t *testing.T) {
 	buffer, err := os.ReadFile("rope_test.txt")
 	if err != nil {
 		log.Fatalf("error parsing file: %v", err)
 	}
-	visited := countVisitedCells(string(buffer))
+	visited := countVisitedCells(string(buffer), 2)
 	assert.Equal(t, 13, visited)
+}
+
+func TestCountVisitedCellsWithTenKnots(t *testing.T) {
+	buffer, err := os.ReadFile("rope_10_test.txt")
+	if err != nil {
+		log.Fatalf("error parsing file: %v", err)
+	}
+	visited := countVisitedCells(string(buffer), 10)
+	assert.Equal(t, 36, visited)
 }
